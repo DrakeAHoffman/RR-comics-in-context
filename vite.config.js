5f0dc9325de338aa3c6fca51eee09bd915baff90
DrakeAHoffman <drakehoffman99@gmail.com>
2023-10-08 11:54:09 -0700

    finished project

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+/**
+ * https://vitejs.dev/config/
+ * @type { import('vite').UserConfig }
+ */
+export default {
+  plugins: [reactRefresh()],
+  server: {
+    hmr: {
+      port: 443,
+    }
+  }
+
+}
